84863782ba7ce7730bd717540836bbd0a23b1359
KUEUNJI <112163447+EunJiKU8151@users.noreply.github.com>
2023-05-30 10:59:38 +0900

    로그인상태유지중

--- src/App.js
+++ src/App.js
@@ -9,18 +9,26 @@ import Toast from "./components/Toast";
 import useToast from './hooks/toast';
 import { useDispatch, useSelector } from 'react-redux';
 import ProtectedRoute from './ProtectedRoute';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { login } from './store/authSlice';
+import LoadingSpinner from "./components/LoadingSpinner";
 
 function App() {
   const toasts = useSelector(state => state.toast.toasts);
   const { deleteToast } = useToast();
+  const [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
   useEffect(() => {
     if(localStorage.getItem('isLoggedIn')) {
       dispatch(login());
-    } 
+    }
+    setLoading(false);
   }, []);
+
+  if(loading) {
+    return <LoadingSpinner />
+  }
+
   return (
     <Router>
     <NavBar />
